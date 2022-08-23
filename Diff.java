8c27126f7e74ced76bb05cdd53002d75c1bd6351
Aishwarya Ghosh <aishwaryaghosh67@gmail.com>
2022-08-23 11:17:28 +0530

    Added implementation for Diff.java

--- Diff.java
+++ Diff.java
@@ -3,8 +3,10 @@ public class Diff {
     public static void main(String args[])
     {
         int a=6;
+        int b=7;
         int c=4;
         System.out.println(a/c);
+        System.out.println(a/b);
     
     }
     
